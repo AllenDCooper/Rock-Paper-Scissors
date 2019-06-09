bf5422bd4d14cad973ef9eb6d2043dc28e382a18
ADC04 <allendcooper@gmail.com>
2019-06-09 19:13:15 -0400

    added scoreboard, buttons to select user, and game logic

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,52 +1,130 @@
-var computerChoices = ["r", "p", "s"];
+// hide player buttons until players have joined game
+$("#player1-buttons").hide();
+$("#player2-buttons").hide();
+$("#player1-scoreboard").hide();
+$("#player2-scoreboard").hide();
 
-    var wins = 0;
-    var losses = 0;
-    var ties = 0;
+// assigning player roles
+var player1Start = $("#player1-start");
+var player2Start = $("#player2-start");
+player1Start = false;
+player2Start = false;
 
-    var directionsText = document.getElementById("directions-text");
-    var user1ChoiceText = document.getElementById("user1choice-text");
-    var user2ChoiceText = document.getElementById("user2choice-text");
-    var winsText = document.getElementById("wins-text");
-    var lossesText = document.getElementById("losses-text");
-    var tiesText = document.getElementById("ties-text");
+$("#player1-start").on("click", function(){
+    player1Start = true;
+    $(this).hide();
+    $("#player1-buttons").show();
+    $("#player1-scoreboard").show();
+    if (player2Start === false) {
+        $("#directions-text").text("Waiting for Player 2 to join");
+    } else {
+        $("#select-player").hide();
+        $("#directions-text").text("Select rock, paper, or scissors.")
+    };
+});
 
-    document.onkeyup = function(event) {
-        var user1Guess = event.key;
-        var user2Guess = computerChoices[Math.floor(Math.random()*computerChoices.length)]
 
-    
-        if ((user1Guess === "r") || (user1Guess === "p") || (user1Guess === "s")) {
-            if ((user1Guess === "r") && (user2Guess === "s")) {
-                wins++
-            }
-            else if ((user1Guess == "r") && (user2Guess === "p")) {
-                losses++
-            }
-            else if ((user1Guess === "p") && (user2Guess === "r")) {
-                wins++
-            }
-            else if ((user1Guess === "p") && (user2Guess === "s")) {
-                losses++
-            }
-            else if ((user1Guess === "s") && (user2Guess === "p")) {
-                wins++
-            }
-            else if ((user1Guess === "s") && (user2Guess === "r")) {
-                losses++
-            }
-            else if (user1Guess === user2Guess) {
-                ties++
-            }
-        };
-    
-        directionsText.textContent = " ";
-        user1ChoiceText.textContent = "You chose " + user1Guess + ".";
-        user2ChoiceText.textContent = "Player 2 chose " + user2Guess + ".";
-        winsText.textContent = "Wins: " + wins;
-        lossesText.textContent = "Losses: " + losses;
-        tiesText.textContent = "Ties: " + ties;
+$("#player2-start").on("click", function(){
+    player2Start = true;
+    $(this).hide();
+    $("#player2-buttons").show();
+    $("#player2-scoreboard").show();
+    if (player1Start === false) {
+        $("#directions-text").text("Waiting for Player 2 to join");
+    } else {
+        $("#select-player").hide();
+        $("#directions-text").text("Select rock, paper, or scissors.")
+    };
+});
+
+
+// RPS game logic
+
+var player1Wins = 0;
+var player1Losses = 0;
+var player1Ties = 0;
+
+var player2Wins = 0;
+var player2Losses = 0;
+var player2Ties = 0;
+
+var player1Choice;
+var player2Choice;
+
+var player1btn = $(".player1-btn");
+var player2btn = $(".player2-btn");
+player1btn = false;
+player2btn = false;
+
+$(".player1-btn").on("click", function(){
+    player1Choice = $(this).attr("data-value")
+    console.log(player1Choice);
+    player1btn = true;
+    $("#directions-text").text("");
+    if (player1btn === true && player2btn === true) {
+        scoreRound();
+    } else {
+        $("#directions-text").text("Waiting for Player 2");
     }
+})
+
+$(".player2-btn").on("click", function(){
+    player2Choice = $(this).attr("data-value")
+    console.log(player2Choice);
+    player2btn = true;
+    $("#directions-text").text("");
+    if (player1btn === true && player2btn === true) {
+        scoreRound();
+    } else {
+        $("#directions-text").text("Waiting for Player 1");
+    };
+});
+
+// scoring function
+function scoreRound() {
+    if ((player1Choice === "rock") || (player1Choice === "paper") || (player1Choice === "scissors")) {
+        if ((player1Choice === "rock") && (player2Choice === "scissors")) {
+            player1Wins++;
+            player2Losses++
+        }
+        else if ((player1Choice == "rock") && (player2Choice === "paper")) {
+            player1Losses++;
+            player2Wins++;
+        }
+        else if ((player1Choice === "paper") && (player2Choice === "rock")) {
+            player1Wins++;
+            player2Losses++;
+        }
+        else if ((player1Choice === "paper") && (player2Choice === "scissors")) {
+            player1Losses++;
+            player2Wins++;
+        }
+        else if ((player1Choice === "scissors") && (player2Choice === "paper")) {
+            player1Wins++;
+            player2Losses++;
+        }
+        else if ((player1Choice === "scissors") && (player2Choice === "rock")) {
+            player1Losses++;
+            player2Wins++;
+        }
+        else if (player1Choice === player2Choice) {
+            player1Ties++;
+            player2Ties++;
+        }
+    };
+    // update player 1 scoreboard
+    $("#1-player1-choice").text("You play: " + player1Choice);
+    $("#1-player2-choice").text("Player 2 plays: " + player2Choice);
+    $("#1-player1-wins").text("Wins: " + player1Wins);
+    $("#1-player1-losses").text("Losses: " + player1Losses);
+    $("#1-player1-ties").text("Ties: " + player1Ties);
+    // update player 2 scoreboard
+    $("#2-player2-choice").text("You play: " + player2Choice);
+    $("#2-player1-choice").text("Player 2 plays: " + player1Choice);
+    $("#2-player2-wins").text("Wins: " + player2Wins);
+    $("#2-player2-losses").text("Losses: " + player2Losses);
+    $("#2-player2-ties").text("Ties: " + player2Ties);
+}
 
   // Your web app's Firebase configuration
   var firebaseConfig = {
